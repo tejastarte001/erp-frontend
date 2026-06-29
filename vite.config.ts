26b8d2e41385e11638d260a576d01b30b2126e6f
Sanket2007010 <your@email.com>
2026-06-29 14:29:23 +0530

    29 june

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4200,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://56.152.66.148.host.secureserver.net:7200',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
